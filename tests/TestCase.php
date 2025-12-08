315ff7ce3917bebce3b7abd45802cc32905d1f84
dennisschirra <d.schirra@parkefficient.de>
2025-12-08 19:02:28 +0100

    Refactor TestCase class by removing unused package provider method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,20 +4,6 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Support\ServiceProvider;
-use Noxsi\GeminiNano\GeminiNanoServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
-{
-    /**
-     * @param  mixed  $app
-     * @return array<int, class-string<ServiceProvider>>
-     */
-    protected function getPackageProviders($app): array
-    {
-        return [
-            GeminiNanoServiceProvider::class,
-        ];
-    }
-}
+abstract class TestCase extends BaseTestCase {}
